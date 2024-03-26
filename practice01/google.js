0802239a2be58dfbf38fbe4e74c9d1dfe64c49ba
kjn0221 <kjn0221@midasit.com>
2024-03-26 20:36:58 +0900

    * 코드 리뷰 반영 - 절대 위치가 아닌 상대 위치로 - 검색과 I'm Feeling Lucky 버튼 방식으로 변경

--- practice01/google.js
+++ practice01/google.js
@@ -1,10 +1,8 @@
 function onSearch(e) {
     var enter_word = document.getElementById("search").value;
-    if (e.keyCode == 13) {
-        var search_word = enter_word;
-        var search_url = "https://www.google.com/search?q=" + search_word;
-        window.open(search_url, "_blank");
-    }
+    var search_url = "https://www.google.com/search?q=" + enter_word;
+    window.open(search_url, "_blank");
+    
 }
 
 window.onload = function () {
@@ -13,4 +11,14 @@ window.onload = function () {
 function spanBtn() {
     var search_url = "https://www.google.com/search?q=" + document.getElementById("search").value;
     window.open(search_url, "_blank");
+}
+
+window.onkeydown = function (e) {
+    if (e.keyCode == 13) {
+        onSearch(e);
+    }
+}
+
+function luckyBtn() {    
+    window.open("https://doodles.google", "_blank");
 }
