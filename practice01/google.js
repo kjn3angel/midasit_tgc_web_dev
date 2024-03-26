45bf07d7bd09325e41155084346b28513e7fc932
kjn0221 <kjn0221@midasit.com>
2024-03-26 15:11:06 +0900

    * 과제 올림

--- practice01/google.js
+++ practice01/google.js
@@ -1,5 +1,16 @@
-function onSearch() {
+function onSearch(e) {
+    var enter_word = document.getElementById("search").value;
+    if (e.keyCode == 13) {
+        var search_word = enter_word;
+        var search_url = "https://www.google.com/search?q=" + search_word;
+        window.open(search_url, "_blank");
+    }
 }
 
 window.onload = function () {
+}
+
+function spanBtn() {
+    var search_url = "https://www.google.com/search?q=" + document.getElementById("search").value;
+    window.open(search_url, "_blank");
 }
